ded2324f681d4c9ba3f383891fd3aa3b8c1c3896
Bogdan Kovalchuk <bogdan.kovalchuk@zenuity.com>
2020-10-18 12:34:25 +0300

    Multiply string solution

--- 43. Multiply Strings/multiply_strings.cpp
+++ 43. Multiply Strings/multiply_strings.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-
 // Source : https://leetcode.com/problems/multiply-strings/
 // Author : Bogdan Kovalchuk
 // Date   : 2020-10-18
 
 /**
- * Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and 
- * num2, also represented as a string.
+ * Given two non-negative integers num1 and num2 represented as strings, return
+ * the product of num1 and num2, also represented as a string.
  *
  * Example 1:
  * Input: num1 = "2", num2 = "3"
@@ -22,6 +20,11 @@
  * Both num1 and num2 do not contain any leading zero, except the number 0 itself.
  * You must not use any built-in BigInteger library or convert the inputs to integer directly.
 **/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 class Solution {
 public:
