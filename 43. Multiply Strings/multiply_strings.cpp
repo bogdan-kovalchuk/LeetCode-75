d584520b6c0cef79ca6f878920d9d46802d3c924
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-10-18 10:38:18 +0300

    Multiply strings solution

--- 43. Multiply Strings/multiply_strings.cpp
+++ 43. Multiply Strings/multiply_strings.cpp
@@ -23,6 +23,7 @@
 
 #include <iostream>
 #include <string>
+// #include <math.h>
 
 using namespace std;
 
@@ -31,15 +32,26 @@ public:
     string multiply(string num1, string num2) {
         if (num1.size() == 1 || num2.size() == 1)
             return to_string(stoi(num1) * stoi(num2));
-         
+
+        int m = min(num1.size(), num2.size())/2;
+
+        string x0, x1, y0, y1;
+        x1 = num1.substr(0, m);
+        x0 = num1.substr(m + 1, num1.back());
+
+        y1 = num2.substr(0, m);
+        y0 = num2.substr(m + 1, num2.back());
+
+        cout << "";
+
         
     }
 };
 
 int main()
 {
-    string num1 = "2";
-    string num2 = "3";
+    string num1 = "233";
+    string num2 = "3345";
     Solution solution;
 
     cout << solution.multiply(num1, num2) << endl;
