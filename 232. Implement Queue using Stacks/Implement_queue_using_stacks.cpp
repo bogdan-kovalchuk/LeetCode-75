dba1477c08d9c52bab12cd486f231611025b6424
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-10-30 16:01:06 +0200

    Implement Queue using Stacks

--- 232. Implement Queue using Stacks/Implement_queue_using_stacks.cpp
+++ 232. Implement Queue using Stacks/Implement_queue_using_stacks.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <stack>
+
+using namespace std;
+
+class EmptyException
+{
+};
 
 class MyQueue
 {
@@ -11,32 +18,51 @@ public:
     /** Push element x to the back of queue. */
     void push(int x)
     {
+        input.push(x);
     }
 
     /** Removes the element from in front of queue and returns that element. */
     int pop()
     {
+        auto out = peek();
+        output.pop();
     }
 
     /** Get the front element. */
     int peek()
     {
+        if (!output.empty())
+        {
+            return output.top();
+        }
+
+        else if (!input.empty())
+        {
+            while (!input.empty())
+            {
+                output.push(input.top());
+                input.pop();
+            }
+            return output.top();
+        }
+        else
+        {
+            throw EmptyException();
+        }
     }
 
     /** Returns whether the queue is empty. */
     bool empty()
     {
+        if (input.empty() && output.empty())
+            return true;
+        return false;
     }
-};
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
+private:
+    stack<int> input;
+    stack<int> output;
+};
 
 int main()
 {
