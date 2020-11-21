5f155654c019b2571d2187df36ce87aa3bef6b2f
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-11-21 11:32:11 +0200

    Implement queue using stacks refactoring

--- 232. Implement Queue using Stacks/Implement_queue_using_stacks.cpp
+++ 232. Implement Queue using Stacks/Implement_queue_using_stacks.cpp
@@ -1,3 +1,7 @@
+// Source : https://leetcode.com/problems/implement-queue-using-stacks/
+// Author : Bogdan Kovalchuk
+// Date   : 2020-11-21
+
 /**
  * Implement a first in first out (FIFO) queue using only two stacks. 
  * The implemented queue should support all the functions of a normal 
