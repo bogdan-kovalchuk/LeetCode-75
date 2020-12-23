61b6b052cf39e1d2ce2a20023e1840a448c39350
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-12-23 20:35:31 +0200

    Lowest Common Ancestor of a Binary Tree

--- 236. Lowest Common Ancestor of a Binary Tree/Lowest_common_ancestor_of_a_binary_tree.cpp
+++ 236. Lowest Common Ancestor of a Binary Tree/Lowest_common_ancestor_of_a_binary_tree.cpp
@@ -15,6 +15,13 @@ class Solution
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
+        if (root == p || root == q || root == nullptr)
+            return root;
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+        if (left && right)
+            return root;
+        return left ? left : right;
     }
 };
 
