a51cbeced53cff93a0bd3e785737ffc808453e56
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-05-01 10:59:07 +0800

    1679 solution

--- 02 - Two Pointers/1679. Max Number of K-Sum Pairs.cpp
+++ 02 - Two Pointers/1679. Max Number of K-Sum Pairs.cpp
@@ -1,15 +1,37 @@
 #include "../leetcode_test.hpp"
 
-class Solution {
+class Solution
+{
 public:
-    int maxOperations(vector<int>& nums, int k) {
-        throw logic_error("Not implemented");
+    int maxOperations(vector<int> &nums, int k)
+    {
+        int i = 0;
+        int j = nums.size() - 1;
+        int maxOps = 0;
+
+        std::sort(nums.begin(), nums.end());
+
+        while (i < j)
+        {
+            if (nums[i] + nums[j] == k)
+            {
+                maxOps++;
+                i++;
+                j--;
+            }
+            else if (nums[i] + nums[j] < k)
+                i++;
+            else
+                j--;
+        }
+        return maxOps;
     }
 };
 
 // LOCAL_TEST_BEGIN
 #ifdef LOCAL_TEST
-int main() {
+int main()
+{
     {
         Solution solution;
         vector<int> nums = vector<int>{1, 2, 3, 4};
