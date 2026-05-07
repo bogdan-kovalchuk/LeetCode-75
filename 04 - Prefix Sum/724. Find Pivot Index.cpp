6e527cc2d35563952cb84ed0374f6113eb780ef2
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-05-07 08:21:28 +0300

    724 solution

--- 04 - Prefix Sum/724. Find Pivot Index.cpp
+++ 04 - Prefix Sum/724. Find Pivot Index.cpp
@@ -1,15 +1,32 @@
 #include "../leetcode_test.hpp"
+#include <numeric>
 
-class Solution {
+class Solution
+{
 public:
-    int pivotIndex(vector<int>& nums) {
-        throw logic_error("Not implemented");
+    int pivotIndex(vector<int> &nums)
+    {
+        int sumLeft = 0;
+        int sumRight = std::accumulate(nums.begin(), nums.end(), 0);
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            sumRight -= nums[i];
+
+            if (sumLeft == sumRight)
+                return i;
+
+            sumLeft += nums[i];
+        }
+
+        return -1;
     }
 };
 
 // LOCAL_TEST_BEGIN
 #ifdef LOCAL_TEST
-int main() {
+int main()
+{
     {
         Solution solution;
         vector<int> nums = vector<int>{1, 7, 3, 6, 5, 6};
